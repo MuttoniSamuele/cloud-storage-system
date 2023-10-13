df63142caa00b6a953ca822b91800d932c10b1d0
Muttoni <muttoni.samuele.studente@itispaleocapa.it>
2023-10-13 06:09:02 +0000

    Add navigation and refresh button

--- public/src/lib/components/FolderControls.svelte
+++ public/src/lib/components/FolderControls.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import Breadcrumb from "./Breadcrumb.svelte";
+  import IconButton from "./IconButton.svelte";
+  import Navigation from "./Navigation.svelte";
 </script>
 
 <!--
 - back, forward
+- refresh
 - breadcrumb
 - upload
 - create folder
 - switch grid/row
 - filter
 -->
-<section>
+<section class="flex">
+  <Navigation />
+  <IconButton icon="ri-restart-line ml-1 mr-2" small />
   <Breadcrumb />
 </section>
---
+++ public/src/lib/components/Navigation.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import IconButton from "./IconButton.svelte";
+</script>
+
+<div class="flex items-center">
+  <IconButton icon="ri-arrow-left-line" />
+  <IconButton icon="ri-arrow-right-line" />
+</div>
