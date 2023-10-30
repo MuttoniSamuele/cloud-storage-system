3ce1335ad866a38b7008065669383a1dd45cf8c0
mutti <imperialvictor15@gmail.com>
2023-10-30 17:31:36 +0100

    Fix not logged in page

--- public/src/lib/components/NotLoggedIn.svelte
+++ public/src/lib/components/NotLoggedIn.svelte
@@ -4,21 +4,19 @@
 </script>
 
 <div class="flex justify-center items-center w-full h-full">
-  <div class="flex flex-col items-center">
-    <i
-      class="ri-account-box-line ri-4x -my-0 text-zinc-400 dark:text-zinc-500"
-    />
+  <div class="flex flex-col items-center mb-14">
+    <i class="ri-account-box-line ri-4x text-zinc-400 dark:text-zinc-500" />
     <span class="mb-5 text-lg text-zinc-700 dark:text-zinc-200">
       You must have an account in order to use this service.
     </span>
     <TextButton
-      text="Sign in"
+      text="Log in"
       marginY
       wide
       on:click={() => API.login("User", "password")}
     />
     <TextButton
-      text="Log in"
+      text="Sign in"
       marginY
       wide
       on:click={() => API.login("User", "password")}
