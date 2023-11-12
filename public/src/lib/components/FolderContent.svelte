7c3b74ede8ecd5e95b2f01160a4a6136f0c1b2e7
mutti <imperialvictor15@gmail.com>
2023-11-12 17:54:51 +0100

    Fix files grid scroll bar on overflow

--- public/src/lib/components/FolderContent.svelte
+++ public/src/lib/components/FolderContent.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import FilesGrid from "./FilesGrid.svelte";
+
+  let availableHeight: number | null = null;
+
+  function handleWindowResize() {
+    availableHeight = null;
+  }
+
+  onMount(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
 </script>
 
-<div class="flex-1 h-full overflow-y-auto">
-  <FilesGrid />
+<div class="h-full overflow-y-auto" bind:offsetHeight={availableHeight}>
+  <div style="height: {availableHeight !== null ? availableHeight : 0}px;">
+    <FilesGrid />
+  </div>
 </div>
