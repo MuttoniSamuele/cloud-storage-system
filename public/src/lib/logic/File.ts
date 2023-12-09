5ed83195b8018c44d20b999a0893751b1afa0a78
mutti <imperialvictor15@gmail.com>
2023-12-09 16:11:17 +0100

    Split file name and extension

--- public/src/lib/logic/File.ts
+++ public/src/lib/logic/File.ts
@@ -2,10 +2,27 @@ import type { FileType } from "./fileUtils";
 import type IFile from "./IFile";
 
 export default class File implements IFile {
+  public readonly name: string;
+  public readonly extension: string | null;
+
   constructor(
-    public readonly name: string,
+    name: string,
     public readonly fileType: FileType,
     public readonly owner: string,
     public readonly lastModified: string
-  ) { }
+  ) {
+    const fileNameArr = name.split(".");
+    // Check if there was at least a "."
+    if (fileNameArr.length >= 2) {
+      // Use the last element as the extension
+      this.extension = fileNameArr.pop() as string;
+      // Join the rest of the name back with "."
+      this.name = fileNameArr.join(".");
+    } else {
+      // Otherwise it only has a name
+      this.name = fileNameArr[0];
+      // And no extension
+      this.extension = null;
+    }
+  }
 }
