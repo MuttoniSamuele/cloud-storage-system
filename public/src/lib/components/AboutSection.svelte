38ba3c7cb96b818508fab44b33d3ebeca008fa98
mutti <imperialvictor15@gmail.com>
2024-04-29 22:48:36 +0200

    Add about section

--- public/src/lib/components/AboutSection.svelte
+++ public/src/lib/components/AboutSection.svelte
@@ -3,7 +3,16 @@
 </script>
 
 <CloudSection title="About the app">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ut dicta
-  quisquam consequatur eligendi nostrum sit praesentium minus cum vitae eveniet
-  assumenda unde, optio inventore omnis totam rem tenetur vel!
+  <p class="mb-2">
+    This app is a simple cloud storage system that was build in svelte and rust.
+  </p>
+  <p class="mb-2">
+    You can check out the code and other project information on the
+    <a
+      class="text-blue-500 underline"
+      href="https://github.com/MuttoniSamuele/cloud-storage-system"
+      target="_blank">github page</a
+    >.
+  </p>
+  <p class="italic">This project was created for educational purposes only.</p>
 </CloudSection>
